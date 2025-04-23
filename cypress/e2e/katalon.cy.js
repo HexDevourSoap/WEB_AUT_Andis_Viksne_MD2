f3b0f76ab07db1afc392313c76947734ffa49189
HexDevourSoap <forgottenbabo@gmail.com>
2025-04-24 00:54:40 +0300

    first scenario except validation

--- cypress/e2e/katalon.cy.js
+++ cypress/e2e/katalon.cy.js
@@ -1,5 +1,45 @@
+import { HomePage } from "../pageObjects/HomePage"
 describe('template spec', () => {
-  it('passes', () => {
+  beforeEach(() => {
     cy.visit('https://katalon-demo-cura.herokuapp.com/')
   })
+    // Make an Appointment
+  it('Make an Appointment', () => {
+    // Click - Make Appointment
+    HomePage.makeAppointmentButton.click();
+    // Set username and password fields with the demo account credentials
+    HomePage.usernameButton.type('John Doe');
+    HomePage.passwordButton.type('ThisIsNotAPassword');
+    // Click - Login
+    HomePage.loginButton.click();
+    // Select a facility from the dropdown
+    HomePage.facilityfield.select('Seoul CURA Healthcare Center');
+    // Check - Apply for hospital readmission
+    HomePage.hospitalReadmission.click();
+    // Select - Medicaid
+    HomePage.programMedicaid.click();
+    // Set Date value by using the widget - 30
+    HomePage.dateField.type('12/30/2023{esc}');
+    // Set comment - CURA Healthcare Service
+    HomePage.commentField.type('CURA Healthcare Service');
+    // Click - Book Appointment
+    HomePage.bookAppointmentButton.click();
+    // Verify - Appointment Confirmation
+    
+
+
+
+  }); 
+
+
+
+
+
+
+
+
+
+
+
+
 })
--- cypress/pageObjects/HomePage.js
+++ cypress/pageObjects/HomePage.js
@@ -1,8 +1,45 @@
-import BasePage from "./BasePage";
+import { BasePage } from "./BasePage";
 export class HomePage extends BasePage {
     static get url() {
       return "/#/";
     }
+    static get makeAppointmentButton() {
+      return cy.get("#btn-make-appointment");
+    }
+    static get usernameButton() {
+        return cy.get("#txt-username");
+      }
+    
+    static get passwordButton() {
+        return cy.get("#txt-password");
+      }
+
+    static get loginButton() {
+        return cy.get("#btn-login");
+      }
+    static get facilityfield() {
+        return cy.get("#combo_facility");
+      }
+    static get hospitalReadmission() {
+            return cy.get("#chk_hospotal_readmission");
+     }
+    static get programMedicaid() {
+        return cy.get("#radio_program_medicaid");
+      }
+    static get dateField() {
+        return cy.get("#txt_visit_date");
+      }
+   
+    
+    static get commentField() {
+        return cy.get("#txt_comment");
+      }
+    static get bookAppointmentButton() {
+        return cy.get("#btn-book-appointment");
+      }
+
+
+
 
 
 
